474812d3b1262627dbd09cda31d4365e115c9ba7
teslo <teslo.knife@gmail.com>
2025-03-26 10:31:06 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Module-2.-Frontend-Lesson-05-Work-1
+
+npm install
+
+npm run dev
---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+import { Todo } from './componets';
+import styles from './app.module.css';
+
+export const App = () => {
+	const [todos, setTodos] = useState([]);
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then((todosData) => todosData.json())
+			.then((loadedTodos) => setTodos(loadedTodos));
+	}, []);
+
+	return (
+		<div className={styles.app}>
+			<div>
+				{todos.map(({ id, title, completed }) => (
+					<Todo key={id} title={title} completed={completed} />
+				))}
+			</div>
+		</div>
+	);
+};
